535413bb5a86422d9b02a3b0d8d386f590208cd5
willie666687 <willie35212736@gmail.com>
2023-01-01 11:09:44 +0800

    update

--- scripts/enemies.gd
+++ scripts/enemies.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
 var slime = preload("res://scenes/slime.tscn")
+onready var avoid_objects = [$"/root/Node2D/player",$"/root/Node2D/scene/campfire"]
 
 func _ready():
 	pass
 func _process(delta):
-	if $slimes.get_child_count() < 15:
-		var avoid_objects = [$"/root/Node2D/player",$"/root/Node2D/scene/campfire"]
+	spawn($slimes,slime)
+	pass
+
+func spawn(monster,monsterScene):
+	if monster.get_child_count() < 150:
 		avoid_objects += $"/root/Node2D/scene/trees".get_children() + $"/root/Node2D/enemies/slimes".get_children() + $"/root/Node2D/scene/stones".get_children()
-		var instance = slime.instance()
+		var instance = monsterScene.instance()
 		var threshold = 1250
 		instance.position.x = rand_range(-10000,10000)
 		instance.position.y = rand_range(-10000,10000)
@@ -20,9 +24,10 @@ func _process(delta):
 			pos = Vector2(instance.position.x - 2960,instance.position.y + 720)
 			var canSpawn = true
 			for i in range(avoid_objects.size()):
-				if pos.distance_to(avoid_objects[i].global_position*10) < threshold:
-					canSpawn = false
+				if is_instance_valid( avoid_objects[i] ):
+					if pos.distance_to(avoid_objects[i].global_position*10) < threshold:
+						canSpawn = false
+						break
 			if canSpawn == true:
 				break
-		$slimes.call_deferred("add_child",instance)
-	pass
+		monster.call_deferred("add_child",instance)
